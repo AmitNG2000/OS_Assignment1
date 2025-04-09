c2b009989acd8b513e7afcf11b1002876df7d1c2
Amit Ner Gaon <amitner@post.bgu.ac.il>
2025-04-09 13:58:37 +0000

    task 2 completed

--- Makefile
+++ Makefile
@@ -133,8 +133,7 @@ UPROGS=\
 	$U/_wc\
 	$U/_zombie\
 	$U/_helloworld\
-	$U/memsize\
-	$U/memsize_test\
+	$U/_memsize_test\
 
 fs.img: mkfs/mkfs README $(UPROGS)
 	mkfs/mkfs fs.img README $(UPROGS)
--- user/memsize_test.c
+++ user/memsize_test.c
@@ -1,16 +1,14 @@
 #include "kernel/types.h"
-#include "kernel/stat.h"
 #include "user/user.h"
-
-#include <stdio.h>
 
 int main() {
     int process_size = memsize();
     printf("Process size: %d bytes\n", process_size);
-    // int* arr = (int *) (malloc(20 * 1024)); // Allocate 20 KB
-    // int new_process_size = memsize();
-    // printf("Process size after malloc: %d bytes\n", new_process_size);
-    // free(arr); // Free 20 KB
-    // printf("Process size after free: %d bytes\n", memsize());
+    int* arr = (int *) (malloc(20 * 1024)); // Allocate 20 KB
+    process_size = memsize();
+    printf("Process size after malloc: %d bytes\n", process_size);
+    free(arr); // Free 20 KB
+    process_size = memsize();
+    printf("Process size after free: %d bytes\n", process_size);
     return 0;
 }
